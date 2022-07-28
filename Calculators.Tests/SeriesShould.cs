50871a03df5c95e0e2f4bfbe0e8e020821b1421d
Jose Antonio Solis Martinez <jose-antonio.solis@atos.net>
2022-07-28 15:41:38 -0500

    Cambiados todos los stubs a mocks reales

--- Calculators.Tests/SeriesShould.cs
+++ Calculators.Tests/SeriesShould.cs
@@ -33,34 +33,20 @@ namespace Calculators.Tests
         public void VerifyFactorial()
         {
             // Arrange
+            Mock<ICalculator> calculatorMock = new Mock<ICalculator>();
+            calculatorMock.Setup(a => a.Mul(It.IsAny<int>(), It.IsAny<int>()))
+                .Returns((int first, int second) => first * second);
+            Series series = new Series(calculatorMock.Object);
             int testNumber = 9;
             int expected = 362880;
             int actual;
-            Mock<ICalculator> calculatorMock = new Mock<ICalculator>();
-            calculatorMock.Setup(a => a.Mul(testNumber, 40320)).Returns(expected);
-            calculatorMock.Setup(a => a.Mul(8, 5040)).Returns(40320);
-            calculatorMock.Setup(a => a.Mul(7, 720)).Returns(5040);
-            calculatorMock.Setup(a => a.Mul(6, 120)).Returns(720);
-            calculatorMock.Setup(a => a.Mul(5, 24)).Returns(120);
-            calculatorMock.Setup(a => a.Mul(4, 6)).Returns(24);
-            calculatorMock.Setup(a => a.Mul(3, 2)).Returns(6);
-            calculatorMock.Setup(a => a.Mul(2, 1)).Returns(2);
-            Series series = new Series(calculatorMock.Object);
 
             // Act
             actual = series.Factorial(testNumber);
 
             // Assert
             Assert.Equal(expected, actual);
-            calculatorMock.Verify(a => a.Mul(9, 40320));
-            calculatorMock.Verify(a => a.Mul(8, 5040));
-            calculatorMock.Verify(a => a.Mul(7, 720));
-            calculatorMock.Verify(a => a.Mul(6, 120));
-            calculatorMock.Verify(a => a.Mul(5, 24));
-            calculatorMock.Verify(a => a.Mul(4, 6));
-            calculatorMock.Verify(a => a.Mul(3, 2));
-            calculatorMock.Verify(a => a.Mul(2, 1));
-
+            calculatorMock.Verify(a => a.Mul(It.IsAny<int>(), It.IsAny<int>()));
         }
     }
 }
